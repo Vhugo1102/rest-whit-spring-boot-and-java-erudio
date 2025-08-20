6fd7358448a91c72cfeffebfb42766601f4aea33
Vitor Hugo da Silva <149020824+Vhugo1102@users.noreply.github.com>
2025-08-20 17:02:44 -0300

    Fist steps in java with spring boot

---
+++ rest-whit-spring-boot-and-java/src/main/java/br/com/vhugo1102/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.vhugo1102.model;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ rest-whit-spring-boot-and-java/src/test/java/br/com/vhugo1102/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.vhugo1102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
